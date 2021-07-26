74cb6d4ee7c3377ceab074dd3e926b3cb818f035
ChadOWGit <chad.estoup@gmail.com>
2021-07-27 00:51:09 +0200

    update DataUtils.java to accept UTF-8 encoding

--- OmegaCore/src/main/java/fr/ChadOW/omegacore/utils/DataUtils.java
+++ OmegaCore/src/main/java/fr/ChadOW/omegacore/utils/DataUtils.java
@@ -3,11 +3,9 @@ package fr.ChadOW.omegacore.utils;
 import fr.ChadOW.api.managers.JedisManager;
 import fr.ChadOW.omegacore.P;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,22 +35,30 @@ public class DataUtils<T> {
 
         List<T> list = new ArrayList<>();
         if (file.length() > 0) {
-            for (String str : file.split(";")) {
-                list.add(JedisManager.getGson().fromJson(str, type));
+            String[] rawData = file.split(";");
+            for (String str : rawData) {
+                T obj = JedisManager.getGson().fromJson(str, type);
+                if (obj != null)
+                    list.add(obj);
             }
         }
         return list;
     }
 
-    public FileReader getFileReader(String URL) throws FileNotFoundException {
-        return new FileReader(P.getInstance().getDataFolder().getPath() + "/data/" + URL);
+    public InputStreamReader getFileReader(String URL) {
+        try {
+            return new InputStreamReader(new FileInputStream(P.getInstance().getDataFolder().getPath() + "/data/" + URL), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String readFile(String URL) throws FileNotFoundException {
         return readFile(getFileReader(URL));
     }
 
-    public String readFile(FileReader file) {
+    public String readFile(InputStreamReader file) {
         StringBuilder result = new StringBuilder();
         try {
             int r = file.read();
@@ -66,17 +72,17 @@ public class DataUtils<T> {
         }
         return result.toString();
     }
-    public FileWriter getFileWriter(String URL) throws IOException {
-        return new FileWriter(P.getInstance().getDataFolder().getPath() + "/data/" + URL);
+    public OutputStreamWriter getFileWriter(String URL) throws IOException {
+        return new OutputStreamWriter(new FileOutputStream(P.getInstance().getDataFolder().getPath() + "/data/" + URL), StandardCharsets.UTF_8);
     }
 
     public void writeFile(String URL, String data) throws IOException {
-        FileWriter file = getFileWriter(URL);
+        OutputStreamWriter file = getFileWriter(URL);
         writeFile(file, data);
         file.close();
     }
 
-    public void writeFile(FileWriter file, String data) throws IOException {
+    public void writeFile(OutputStreamWriter file, String data) throws IOException {
         file.write(data);
     }
 }
